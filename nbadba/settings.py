996e896f63da7c066c24ebd2a24f58420dbd5da7
ayushkarnawat <ayush.karnawat97@gmail.com>
2021-09-29 10:29:58 -0400

    Combine statics and templates

--- nbadba/settings.py
+++ nbadba/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'nbadba.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, "templates") ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/backend/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'backend/static'),) # new
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ] # new
